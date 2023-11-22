288960944936924494c1ee5b812ca784e2dc9145
domenico-mandolino <domenico-mandolino@laplateforme.io>
2023-11-22 12:43:01 +0100

    job 07

---
+++ jour03/job07/function_languages.py
@@ -0,0 +1,12 @@
+def languages (language):
+    if language == "JavaScript":
+        print("tu es un développeur web")
+    elif language == "python":
+        print("tu es un développeur IA")
+    elif language == "java":
+        print("tu es un développeur logiciel")
+    else:
+        print("language non recounu")
+
+
+languages("java")
