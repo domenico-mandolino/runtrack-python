7c752e3290657945be220e717ce83a81b64f5ba2
domenico-mandolino <domenico-mandolino@laplateforme.io>
2023-11-22 12:33:48 +0100

    job 06

---
+++ jour03/job06/Function_Positive_Nagatif.py
@@ -0,0 +1,8 @@
+def PositifOrNegatif (number):
+    if number >= 0:
+        print("le ciffre", number," est positif")
+    else:
+        number < 0
+        print("le ciffre", number, "est negatif")
+
+PositifOrNegatif(-3)
