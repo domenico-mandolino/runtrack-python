d013cc76407178a5b7f51a9676a664381a58ebfd
domenico-mandolino <domenico-mandolino@laplateforme.io>
2023-11-25 13:16:32 +0100

    job03

---
+++ jour04/job03/ajuter_melon.py
@@ -0,0 +1,8 @@
+def ajouter_melon():
+    fruits = ["pomme", "cerise", "orange"]
+    fruits.append("melon")
+    return fruits
+
+# Appeler la fonction et afficher le résultat
+resultat = ajouter_melon()
+print(resultat)
