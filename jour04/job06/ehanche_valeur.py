2b1b80ab6f7427c543a4d73900b4ee89b946b15a
domenico-mandolino <domenico-mandolino@laplateforme.io>
2023-11-26 22:22:37 +0100

    job06 update

--- jour04/job06/ehanche_valeur.py
+++ jour04/job06/ehanche_valeur.py
@@ -3,7 +3,7 @@ L = [1, 2, 3, 4, 5]
 def echange_valeur(liste):
     liste = L
     #verification que la liste n'est pas vive
-    if len(liste) >= 1:
+    if len(liste) >= 2:
       #échange des valeurs de la première et de la dernière case
       liste[0], liste[4] = liste[4], liste[0]
    
