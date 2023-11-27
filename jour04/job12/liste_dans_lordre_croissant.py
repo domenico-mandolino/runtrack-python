365fff2abff4bcc04300bee3463c9f037d7ad46c
domenico-mandolino <domenico-mandolino@laplateforme.io>
2023-11-27 13:04:09 +0100

    Maj job12 ajout job13

---
+++ jour04/job12/liste_dans_lordre_croissant.py
@@ -0,0 +1,9 @@
+L = [8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91]
+
+def liste_dans_ordre(liste):
+    
+    liste = L
+    liste_triee = sorted(L)
+    print("Liste après le tri :", liste_triee)
+
+liste_dans_ordre(L)
