9dd40d099414d7f78fa205eae0098031c66c4fde
domenico-mandolino <domenico-mandolino@laplateforme.io>
2023-11-27 23:30:56 +0100

    creation dossier JOur5 + Job02

---
+++ jour05/job02/dessin.recteangle.py
@@ -0,0 +1,13 @@
+
+
+def draw_rectangle(width, height):
+    for i in range(height):
+        if i == 0 or i == height - 1:
+            # Première et dernière ligne du rectangle
+            print('-' * width)
+        else:
+            # Lignes intérieures du rectangle
+            print('|' + ' ' * (width - 2) + '|')
+
+# Appeler la fonction avec les paramètres (width, height)
+draw_rectangle(10, 3)
